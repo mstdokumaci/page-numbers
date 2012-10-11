514b4c6841bcb50e8080170bcb45ecac9ef90cf7
Mustafa Dokumacı <mustafa@foraa.com>
2012-10-12 00:31:35 +0300

    steps get smaller when they get closer to current page

--- demo.php
+++ demo.php
@@ -3,6 +3,7 @@
 
 	echo "<pre>\n";
 	echo implode(' ', page_numbers(321, 893, 12)) . "\n";
-	echo implode(' ', page_numbers(67, 93, 10)) . "\n";
-	echo implode(' ', page_numbers(11, 36, 15)) . "\n";
+	echo implode(' ', page_numbers(67, 93, 15)) . "\n";
+	echo implode(' ', page_numbers(11, 36, 10)) . "\n";
+	echo implode(' ', page_numbers(30, 68, 10)) . "\n";
 	echo "</pre>\n";
--- page_numbers.php
+++ page_numbers.php
@@ -2,7 +2,7 @@
 	function page_numbers ($current_page, $page_count, $link_count) {
 		if ($link_count<10) {$link_count=10;}
 		$page_links=array(1=>true, $page_count=>true);
-		for ($i=$current_page-2;$i<$current_page+3;$i++) {
+		for ($i=$current_page-1;$i<$current_page+2;$i++) {
 			if ($i<1 || $i>$page_count) {continue;}
 			$page_links[$i]=true;
 		}
@@ -16,9 +16,14 @@
 			$left_step=$left/($left_step+1);
 			$power=floor(log10($left_step));
 			$round=(($left_step/pow(10, $power))<5) ? pow(10, $power) : pow(10, $power+1);
-			for ($i=1;$i<$current_page-2;$i+=$left_step) {
+			for ($i=1;$i<$current_page-1;$i+=$left_step) {
 				$i=round($i/$round)*$round;
 				if ($i<1 || $i>$page_count) {continue;}
+				$page_links[$i]=true;
+			}
+			$i=end(array_keys($page_links));
+			if ($current_page-$i>$left_step/2) {
+				$i+=$round/2;
 				$page_links[$i]=true;
 			}
 		}
@@ -27,9 +32,14 @@
 			$right_step=$right/($right_step+1);
 			$power=floor(log10($right_step));
 			$round=(($right_step/pow(10, $power))<5) ? pow(10, $power) : pow(10, $power+1);
-			for ($i=$current_page+2;$i<$page_count;$i+=$right_step) {
+			for ($i=$page_count;$i>$current_page+1;$i-=$right_step) {
 				$i=round($i/$round)*$round;
 				if ($i<1 || $i>$page_count) {continue;}
+				$page_links[$i]=true;
+			}
+			$i=end(array_keys($page_links));
+			if ($i-$current_page>$right_step/2) {
+				$i-=$round/2;
 				$page_links[$i]=true;
 			}
 		}
