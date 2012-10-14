40c7f7d45521f20483fcc7f9702cd30f1d8a977b
Mustafa Dokumacı <mustafa@foraa.com>
2012-10-14 22:24:49 +0300

    better display

--- demo.php
+++ demo.php
@@ -2,8 +2,9 @@
 	require_once 'page_numbers.php';
 
 	echo "<pre>\n";
-	echo implode(' ', page_numbers(321, 893, 12)) . "\n";
-	echo implode(' ', page_numbers(67, 93, 15)) . "\n";
-	echo implode(' ', page_numbers(11, 36, 10)) . "\n";
-	echo implode(' ', page_numbers(30, 68, 10)) . "\n";
+	echo implode(' | ', page_numbers(17, 67, 15)) . "\n";
+	echo implode(' | ', page_numbers(321, 893, 12)) . "\n";
+	echo implode(' | ', page_numbers(67, 93, 15)) . "\n";
+	echo implode(' | ', page_numbers(11, 36, 10)) . "\n";
+	echo implode(' | ', page_numbers(30, 68, 10)) . "\n";
 	echo "</pre>\n";
