52b92c40d0e7c03d9a1df635565e506ec9609178
Mustafa Dokumacı <mustafa@foraa.com>
2012-10-17 14:08:45 +0300

    Update page_numbers.php

--- page_numbers.php
+++ page_numbers.php
@@ -6,9 +6,9 @@
 			if ($i<1 || $i>$page_count) {continue;}
 			$page_links[$i]=true;
 		}
-		$left=$current_page-4; if ($left<1) {$left=false;}
-		$right=$page_count-$current_page-4; if ($right<1) {$right=false;}
-		$available=$link_count-count($page_links);
+		$left=$current_page-2; if ($left<1) {$left=false;}
+		$right=$page_count-$current_page-2; if ($right<1) {$right=false;}
+		$available=$link_count-count($page_links)-2;
 		$left_step=($left) ? $left/($left+$right)*$available : false;
 		$right_step=($right) ? $right/($left+$right)*$available : false;
 		if ($left_step) {
