34572635a1d1886ee1af644538e0a5aa370194ea
Mustafa Dokumacı <mustafa@foraa.com>
2012-10-19 00:41:01 +0300

    better example

--- demo.php
+++ demo.php
@@ -6,5 +6,5 @@
 	echo implode(' | ', page_numbers(321, 893, 12)) . "\n";
 	echo implode(' | ', page_numbers(67, 93, 15)) . "\n";
 	echo implode(' | ', page_numbers(11, 36, 10)) . "\n";
-	echo implode(' | ', page_numbers(30, 68, 10)) . "\n";
+	echo implode(' | ', page_numbers(30, 83, 10)) . "\n";
 	echo "</pre>\n";
